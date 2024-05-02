1ddba69d1b7d05e7c86cf5438d3807ff6f5661b7
Mohamed Sameh <167581011+mohamedsameh20@users.noreply.github.com>
2024-05-03 02:58:04 +0300

    Update Cart.java
    Exceptions added

--- Cart.java
+++ Cart.java
@@ -4,12 +4,26 @@ public class Cart {
 	private ArrayList<Product> cartProducts;
 	Cart(){}
 	public Cart(ArrayList<Product> cartProducts) {
+		
+		if (cartProducts == null) {	
+       		throw new IllegalArgumentException("Cart products can't be null !!");}
+		
 		this.cartProducts = cartProducts;
 	}
 	public void addProducts(Product product) {
+		
+		if (product == null) {
+		throw new IllegalArgumentException("Product can't be null !!");}
+		
 		cartProducts.add(product);
 	}
 	public void removeProduct(Product product) {
+		
+		if (product == null) {
+		throw new IllegalArgumentException("Product can't be null !!");}
+		if (!cartProducts.contains(product)) {
+		throw new IllegalArgumentException("Product not found !!");}
+		
 		cartProducts.remove(product);
 	}
 	public double calculateTotal() {
@@ -23,6 +37,10 @@ public class Cart {
 		return cartProducts;
 	}
 	public void setCartProducts(ArrayList<Product> cartProducts) {
+		
+		if (cartProducts == null) {
+		throw new IllegalArgumentException("Cart products can't be null !!");}
+		
 		this.cartProducts = cartProducts;
 	}
 }
