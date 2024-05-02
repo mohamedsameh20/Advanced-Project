1c290a28571ce5b77c5f478d8ca5c486a0f0aed6
Mohamed Sameh <167581011+mohamedsameh20@users.noreply.github.com>
2024-05-03 02:56:35 +0300

    Update Book.java
    - Exceptions added.
    - Changed the return type of getiD() from int to String.

--- Book.java
+++ Book.java
@@ -1,26 +1,39 @@
 package p1;
 import java.util.Date;
-public class Book extends Product{
-	private String writer;
-	public Book(String pn, double p, int noa, int iD, String m, boolean hd, double dv, Date de, String w) {
-	  super(pn, p, noa, iD, m, hd,dv, de);
-	  this.writer=w;
-	}
-	public String getWriter() {
-		return writer;
-	}
-	public void setWriter(String writer) {
-		this.writer = writer;
-	}
-	public void displayInfo() {
-		System.out.println("Book name is: "+getProductName()+", of ID: "+getiD()+", Its price = "+getPrice()+
-		", number of available: "+getNumberOfAvailable()+", manufacturer: "+getManufacturer()+
-		", does it have discount? --> "+isHasDiscount()+", of "+getDiscountValue()*getPrice()+" $"+
-		", its expiry date: "+getDiscountExpiry()+", writer: "+getWriter());
-	}
-	@Override
+
+public class Book extends Product {
+    private String writer;
+
+    public Book(String pn, double p, int noa, String m, boolean hd, double dv, 
+		Date de, String w) throws Exception {
+        super(pn, p, noa, m, hd, dv, de);
+	    
+        if (w == null || w.isBlank() || w.isEmpty()) {
+        throw new Exception("Writer name can't be empty !!");}
+	    
+        this.writer = w;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) throws Exception {
+	    
+        if (writer == null || writer.isBlank() || writer.isEmpty()) {
+        throw new Exception("Writer name can't be empty !!");}
+	    
+        this.writer = writer;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Book name is: "+getProductName()+", of ID: "+getiD()+", Its price = "+getPrice()+
+        ", number of available: "+getNumberOfAvailable()+", manufacturer: "+getManufacturer()+
+        ", does it have discount? --> "+getHasDiscount()+", of "+getDiscountValue()*getPrice()+" $"+
+        ", its expiry date: "+getDiscountExpiry()+", writer: "+getWriter());
+    }
+
 	public void makeDiscount(double discount, Product discountProduct) {
-		super.makeDiscount(discount, discountProduct);
 	}
-	
 }
